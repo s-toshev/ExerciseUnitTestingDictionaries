174c9b0775e2bbfce8436e306da4534923b79b2f
s-toshev <simeon.toshev@yahoo.bg>
2024-02-19 08:07:10 +0200

    Add project files.

---
+++ TestApp.Tests/CountCharactersTests.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+
+using System;
+using System.Collections.Generic;
+
+namespace TestApp.Tests;
+
+public class CountCharactersTests
+{
+    [Test]
+    public void Test_Count_WithEmptyList_ShouldReturnEmptyString()
+    {
+        // Arrange
+        List<string> input = new();
+
+        // Act
+        string actual = CountCharacters.Count(input);
+        string expected = string.Empty;
+        // Assert
+        Assert.AreEqual(expected, actual);
+
+    }
+
+    // TODO: finish test
+    [Test]
+    public void Test_Count_WithNoCharacters_ShouldReturnEmptyString()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        //Assert.That(result, Is.Empty);
+    }
+
+    [Test]
+    public void Test_Count_WithSingleCharacter_ShouldReturnCountString()
+    {
+        // TODO: finish test
+    }
+
+    [Test]
+    public void Test_Count_WithMultipleCharacters_ShouldReturnCountString()
+    {
+        // TODO: finish test
+    }
+
+    [Test]
+    public void Test_Count_WithSpecialCharacters_ShouldReturnCountString()
+    {
+        // TODO: finish test
+    }
+}
